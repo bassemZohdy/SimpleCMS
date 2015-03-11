7b980d0d1063785a599f1c84cd2ff29ac7ff6222
bassem.zohdy <bassem.zohdy@gmail.com>
2015-03-11 15:07:11 -0400

    fix using domain

--- WebApp/Gruntfile.js
+++ WebApp/Gruntfile.js
@@ -71,13 +71,13 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       proxies: [
         {
             context: '/api',
-            host: 'localhost',
+            host: '0.0.0.0',
             port: 3000,
             https: false,
             changeOrigin: true,
@@ -181,7 +181,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       options: {
-        cwd: '<%= yeoman.app %>'
+        //cwd: '<%= yeoman.app %>'
       },
       app: {
         src: ['<%= yeoman.app %>/index.html'],
--- WebApp/app/scripts/services/resourceService.js
+++ WebApp/app/scripts/services/resourceService.js
@@ -4,13 +4,13 @@ var resourceServices = angular.module('resourceServices', ['ngResource']);
 
 resourceServices.factory('Content', ['$resource',
     function($resource){
-        return $resource('http://localhost:9000/api/content/:id', { id: '@_id' }, {
+        return $resource('/api/content/:id', { id: '@_id' }, {
             update: {method:'PUT', isArray: false},
             count: {method:'GET',isArray: false,params:{count:true}}
         });
     }]);
 resourceServices.factory('Tag', ['$resource',
     function($resource){
-        return $resource('http://localhost:9000/api/tag/:id', { id: '@_id' }, {
+        return $resource('/api/tag/:id', { id: '@_id' }, {
         });
     }]);
